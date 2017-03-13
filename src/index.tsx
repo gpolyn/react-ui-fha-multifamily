0299dd217121c9f5fe58bde4d934753ac6fce891
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-03-13 14:09:32 -0400

    add commercial incomes using OtherIncome component

--- src/index.tsx
+++ src/index.tsx
@@ -15,18 +15,28 @@ let otherIncomes = [
   {id:1, totalMonthlyIncome: 56, monthlyRent: 8765, usage: 'blah', squareFeet: 31}
 ];
 
-function onSave(something: any) { console.log('onSaveFromIndex', something); };
-function onDestroy(id: any) { console.log('onDestroy', id); };
+let commercialIncomes = [
+  {id:0, totalMonthlyIncome: 822, monthlyRent: 822, usage: 'a commercial use', squareFeet: 302},
+  {id:1, totalMonthlyIncome: 890, monthlyRent: 890, usage: 'some commercial use', squareFeet: 61}
+];
 
 function deleteOtherIncome(id: any) {
-  console.log('deleteOtherIncome', id)
   otherIncomes = incomes(otherIncomes, actions.deleteIncome(id));
   render();
 };
 
 function addOtherIncome(income: any) {
-  console.log('addOtherIncome', income)
   otherIncomes = incomes(otherIncomes, actions.addIncome(income));
+  render();
+};
+
+function deleteCommercialIncome(id: any) {
+  commercialIncomes = incomes(commercialIncomes, actions.deleteIncome(id));
+  render();
+};
+
+function addCommercialIncome(income: any) {
+  commercialIncomes = incomes(commercialIncomes, actions.addIncome(income));
   render();
 };
 
@@ -37,16 +47,36 @@ const otherIncomeCSS = {
   squareFeetInputName: 'other-residential-square-feet',
   monthlyRentInputName: 'other-residential-monthly-rent',
   incomeSourceContainerName: 'other-residential-income'
-}
+};
+const commercialIncomeCSS = {
+  newIncomeContainerName: 'new-commercial-income',
+  usageInputName: 'commercial-use usage',
+  squareFeetInputName: 'commercial-square-feet',
+  monthlyRentInputName: 'commercial-monthly-rent',
+  incomeSourceContainerName: 'commercial-income'
+};
 
 function render() {
+  const totalGross = otherIncomes.concat(commercialIncomes).map(inc => inc.totalMonthlyIncome).reduce((total, income) =>
+    total + income,
+    0
+  );
+  console.log('gross other', totalGross);
   ReactDOM.render(
-    <OtherIncome
-     incomes={otherIncomes}
-     onDestroy={deleteOtherIncome}
-     onSave={addOtherIncome}
-     css={otherIncomeCSS}
-     />,
+    <div>
+      <OtherIncome
+       incomes={commercialIncomes}
+       onDestroy={deleteCommercialIncome}
+       onSave={addCommercialIncome}
+       css={commercialIncomeCSS}
+       />
+      <OtherIncome
+       incomes={otherIncomes}
+       onDestroy={deleteOtherIncome}
+       onSave={addOtherIncome}
+       css={otherIncomeCSS}
+       />
+     </div>,
      root
   );
 }
