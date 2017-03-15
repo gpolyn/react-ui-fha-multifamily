60d785d33da1dd57dac687c2ba3bb80118ae85e4
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-03-15 13:01:05 -0400

    added MSA component

---
+++ src/app/project/MSA.spec.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import * as TestUtils from 'react-addons-test-utils';
+import {MSA} from './MSA';
+
+function setup(propOverrides: any) {
+  const props = Object.assign({
+    onChange: jasmine.createSpy('onChange'),
+  }, propOverrides);
+
+  const renderer = TestUtils.createRenderer();
+
+  renderer.render(<MSA {...props}/>);
+
+  let output = renderer.getRenderOutput();
+
+  return {
+    props,
+    output,
+    renderer
+  };
+}
+
+describe('MSA', () => {
+  it('initial render', () => {
+    const initialValue = 'Phildelphia, PA';
+    const {output} = setup({value: initialValue});
+    expect(output.type).toBe('div');
+    expect(output.props.id).toBe('metropolitan-area');
+    expect(output.props.children.type).toBe('label');
+    expect(output.props.children.props.children[0]).toBe('Metropolitan Statistical Area Waiver:');
+
+    expect(output.props.children.props.children[1].type).toBe('select');
+    expect(output.props.children.props.children[1].props.name).toBe('msa');
+    expect(output.props.children.props.children[1].props.id).toBe('high-cost-setting');
+    expect(output.props.children.props.children[1].props.value).toBe(initialValue);
+    expect(output.props.children.props.children[1].props.children.length).toBe(80);
+    output.props.children.props.children[1].props.children.forEach(opt =>
+      expect(opt.type).toBe('option')
+    );
+  });
+  it('should call onChange for select onChange', () => {
+    const {output, props} = setup({});
+    output.props.children.props.children[1].props.onChange({});
+    expect(props.onChange).toHaveBeenCalled();
+  });
+});
---
+++ src/app/project/MSA.tsx
@@ -0,0 +1,102 @@
+import * as React from 'react';
+
+const msaOptions = [
+	'maximum waiver',
+	'standard waiver',
+	'no waiver',
+	'Albany, NY',
+	'Albuquerque, NM',
+	'Anchorage, AK',
+	'Atlanta, GA',
+	'Baltimore, MD',
+	'Bangor, ME',
+	'Birmingham, AL',
+	'Boise, ID',
+	'Boston, MA',
+	'Buffalo, NY',
+	'Burlington, VT',
+	'Camden, NJ',
+	'Casper, WY',
+	'Charleston, WV',
+	'Chicago, IL',
+	'Cincinnati, OH',
+	'Cleveland, OH',
+	'Columbia, SC',
+	'Columbus, OH',
+	'Dallas, TX',
+	'Denver, CO',
+	'Des Moines, IA',
+	'Detroit, MI',
+	'Fargo, ND',
+	'Fort Worth, TX',
+	'Grand Rapids, MI',
+	'Greensboro, NC',
+	'Hartford, CT',
+	'Helena, MT',
+	'Honolulu, HI',
+	'Houston, TX',
+	'Indianapolis, IN',
+	'Jackson, MS',
+	'Jacksonville, FL',
+	'Kansas City, KS',
+	'Knoxville, TN',
+	'Las Vegas, NV',
+	'Little Rock, AR',
+	'Los Angeles, CA',
+	'Louisville, KY',
+	'Lubbock, TX',
+	'Manchester, NH',
+	'Memphis, TN',
+	'Miami, FL',
+	'Milwaukee, WI',
+	'Minneapolis, MN',
+	'Nashville, TN',
+	'New Orleans, LA',
+	'New York, NY',
+	'Newark, NJ',
+	'Oklahoma City, OK',
+	'Omaha, NE',
+	'Philadelphia, PA',
+	'Phoenix, AZ',
+	'Pittsburgh, PA',
+	'Portland, OR',
+	'Providence, RI',
+	'Richmond, VA',
+	'Sacramento, CA',
+	'Salt Lake City, UT',
+	'San Antonio, TX',
+	'San Diego, CA',
+	'San Francisco, CA',
+	'San Juan, PR',
+	'Santa Ana, CA',
+	'Seattle, WA',
+	'Shreveport, LA',
+	'Sioux Falls, SD',
+	'Spokane, WA',
+	'Springfield, IL',
+	'St. Louis, MO',
+	'Tampa, FL',
+	'Topeka, KS',
+	'Tulsa, OK',
+	'US Virgin Islands, US Virgin Islands',
+	'Washington, DC',
+	'Wilmington, DE',
+];
+
+export class MSA extends React.PureComponent<any, any> {
+  render() {
+    const options = msaOptions.map(msa =>
+      <option key={msa} value={msa}>{msa}</option>
+    );
+    return (
+      <div id='metropolitan-area'>
+        <label>
+          Metropolitan Statistical Area Waiver:
+        <select id='high-cost-setting' name='msa' value={this.props.value} onChange={this.props.onChange}>
+          {options}
+        </select>
+        </label>
+      </div>
+    );
+  }
+}
