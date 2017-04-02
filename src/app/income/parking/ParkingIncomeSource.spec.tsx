53b3c026264024d0c231d9ed6afbab11eae1f884
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-04-02 10:53:47 -0400

    refactored ParkingIncomeSource to tr. td

--- src/app/income/parking/ParkingIncomeSource.spec.tsx
+++ src/app/income/parking/ParkingIncomeSource.spec.tsx
@@ -29,39 +29,39 @@ function setup(propOverrides: any) {
 describe('ParkingIncomeSource', () => {
   it('initial render', () => {
     const {output, props} = setup({});
-    expect(output.type).toBe('div');
+    expect(output.type).toBe('tr');
     expect(output.props.className).toBe(props.incomeSourceContainerName);
     const [spaces, isIndoor, squareFeet, fee, deleteBtn] = output.props.children;
 
-    expect(spaces.type).toBe('div');
+    expect(spaces.type).toBe('td');
     expect(spaces.props.className).toBe('spaces');
     expect(spaces.props.children.type).toBe('div');
     const spacesContent = spaces.props.children.props;
     expect(spacesContent.className).toBe('display');
     expect(spacesContent.children).toBe(props.spaces);
 
-    expect(isIndoor.type).toBe('div');
+    expect(isIndoor.type).toBe('td');
     expect(isIndoor.props.className).toBe('indoor-or-outdoor');
     expect(isIndoor.props.children.type).toBe('div');
     const parkingStyleContent = isIndoor.props.children.props;
     expect(parkingStyleContent.className).toBe('display');
     expect(parkingStyleContent.children).toBe('indoor');
 
-    expect(squareFeet.type).toBe('div');
+    expect(squareFeet.type).toBe('td');
     expect(squareFeet.props.className).toBe('total-square-feet');
     expect(squareFeet.props.children.type).toBe('div');
     const squareFeetContent = squareFeet.props.children.props;
     expect(squareFeetContent.className).toBe('display');
     expect(squareFeetContent.children).toBe(props.totalSquareFeet);
 
-    expect(fee.type).toBe('div');
+    expect(fee.type).toBe('td');
     expect(fee.props.className).toBe('monthly-fee');
     expect(fee.props.children.type).toBe('div');
     const feeContent = fee.props.children.props;
     expect(feeContent.className).toBe('display');
     expect(feeContent.children).toBe(props.monthlyFee);
 
-    expect(deleteBtn.type).toBe('div');
+    expect(deleteBtn.type).toBe('td');
     expect(deleteBtn.props.className).toBe('delete-container');
     expect(deleteBtn.props.children.type).toBe('button');
     const deleteContent = deleteBtn.props.children.props;
--- src/app/income/parking/ParkingIncomeSource.tsx
+++ src/app/income/parking/ParkingIncomeSource.tsx
@@ -6,33 +6,33 @@ export class ParkingIncomeSource extends React.PureComponent<any, {}> {
   render() {
     const deleteCssClasses = 'parking-income-destroy destroy-item';
     return (
-      <div className={this.props.incomeSourceContainerName}>
-        <div className='spaces'>
+      <tr className={this.props.incomeSourceContainerName}>
+        <td className='spaces'>
           <div className='display'>
             {this.props.spaces}
           </div>
-        </div>
-        <div className='indoor-or-outdoor'>
+        </td>
+        <td className='indoor-or-outdoor'>
           <div className='display'>
             {this.props.isIndoor ? 'indoor' : 'outdoor'}
           </div>
-        </div>
-        <div className='total-square-feet'>
+        </td>
+        <td className='total-square-feet'>
           <div className='display'>
             {this.props.totalSquareFeet}
           </div>
-        </div>
-        <div className='monthly-fee'>
+        </td>
+        <td className='monthly-fee'>
           <div className='display'>
             {this.props.monthlyFee}
           </div>
-        </div>
-        <div className='delete-container'>
+        </td>
+        <td className='delete-container'>
           <button className={deleteCssClasses} onClick={ () => {this.props.onDelete(this.props.id)} }>
             delete
           </button>
-        </div>
-      </div>
+        </td>
+      </tr>
     );
   }
 }
